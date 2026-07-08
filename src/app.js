0eae7adb05a55c39581afdabd95721396616d737
githubrezaul1990 <rezaulpust.19.cse@gmail.com>
2026-07-08 16:15:39 +0600

    Render backend upload images safely

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(cors({ origin: env.corsOrigin, credentials: true }));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
